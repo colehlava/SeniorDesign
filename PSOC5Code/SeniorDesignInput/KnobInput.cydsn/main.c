845b8dc5263ecca486ee4a6d01724c41e48faedb
unknown <jdlamber@ucsc.edu>
2020-03-02 17:08:59 -0800

    created PSOC project

---
+++ PSOC5Code/SeniorDesignInput/KnobInput.cydsn/main.c
@@ -0,0 +1,17 @@
+/* The knob interface that sends the data to USB for the RPi */
+
+#include "project.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
